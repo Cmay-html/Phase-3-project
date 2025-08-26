4da25c4d034166a779698d8eca26a1c86f749549
cynthia <cynthialairumbe@gmail.com>
2025-08-26 13:53:49 +0300

    Data base schema

---
+++ models.py
@@ -0,0 +1,41 @@
+from sqlalchemy import Column,Integer,DateTime,String,ForeignKey,create_engine,func
+from sqlalchemy.orm import declarative_base,relationship,sessionmaker
+
+Base = declarative_base()
+engine = create_engine("sqlite:///database.db")
+Session = sessionmaker(bind=engine)
+session = Session()
+
+class Employee(Base):
+    __tablename__ = "employees"
+    id = Column(Integer,primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    email = Column(String)
+    department_id = Column(Integer,ForeignKey('departments.id'))
+    position_id = Column(Integer,ForeignKey('positions.id'))
+    hire_date = Column(DateTime,default=func.now())
+
+    position = relationship('Position', back_populates="employee")
+    department = relationship('Department', back_populates="employee")
+
+
+class Department(Base):
+    __tablename__ = "departments"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    description = Column(String)
+
+    employee = relationship('Employee', back_populates="department")
+
+
+
+
+class Position(Base):
+    __tablename__ = "positions"
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    description = Column(String)
+
+    employee = relationship('Employee', back_populates="position")
+
